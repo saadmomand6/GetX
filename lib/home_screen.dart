22b4055cae52ab5766419e11cac99e413d10f773
saadmomand6 <saadmohammad608@gmail.com>
2023-11-03 19:38:23 +0500

    get dailog and change theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,7 +16,70 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('GetX'),
       ),
       body: Column(
-        children: [],
+        children: [
+          Card(
+            child: ListTile(
+              title: const Text('this is for my dailog alert'),
+              subtitle: const Text('this is for my dailog alert'),
+              onTap: () {
+                Get.defaultDialog(
+                  title: 'GetX Dailog',
+                  titlePadding: const EdgeInsets.only(top: 10),
+                  //middleText: 'this is middle text of default dailog',
+                  content: const Column(
+                    children: [
+                      Text('dathis is content text of default dailogta')
+                    ],
+                  ),
+                  contentPadding: const EdgeInsets.all(10),
+                  //built in
+                  // textConfirm: 'confirm dailog',
+                  // textCancel: 'cancel dailog',
+                  //custom
+                  confirm: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('confirm dailog')),
+                  cancel: TextButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: const Text('cancel dailog')),
+                );
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: const Text('this is for my bottom sheet'),
+              subtitle: const Text('this is for my bottom sheet'),
+              onTap: () {
+                Get.bottomSheet(Container(
+                  color: Colors.pink,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.light_mode),
+                        title: const Text('light mode'),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.light());
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.dark_mode),
+                        title: const Text('dark mode'),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.dark());
+                        },
+                      )
+                    ],
+                  ),
+                ));
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -26,8 +89,8 @@ class _HomeScreenState extends State<HomeScreen> {
             snackStyle: SnackStyle.FLOATING,
             icon: const Icon(Icons.abc),
             onTap: (snack) {},
-            mainButton: TextButton(onPressed: (){},
-            child: const  Text('getx main button')),
+            mainButton: TextButton(
+                onPressed: () {}, child: const Text('getx main button')),
             backgroundColor: Colors.yellow,
             snackPosition: SnackPosition.BOTTOM,
           );
