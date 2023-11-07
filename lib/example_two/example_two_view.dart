9c6fe9db77c3fea5ebfa2703b093e265fddebbbf
saadmomand6 <saadmohammad608@gmail.com>
2023-11-07 13:32:09 +0500

    getx example two

---
+++ lib/example_two/example_two_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/example_two/example_two_controller.dart';
+
+class ExampleTwoView extends StatefulWidget {
+  const ExampleTwoView({super.key});
+
+  @override
+  State<ExampleTwoView> createState() => _ExampleTwoViewState();
+}
+
+class _ExampleTwoViewState extends State<ExampleTwoView> {
+  final ExampleTwoController exampletwocontroller = Get.put(ExampleTwoController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GetX Counter View'),
+      ),
+      body: Column(
+        children: [
+          Obx((){
+            return Container(
+              height: 50,
+              width: 50,
+              color: Colors.red.withOpacity(exampletwocontroller.opacity.value),
+            );
+          }
+          ),
+          Obx((){
+            return  Slider(
+                value: exampletwocontroller.opacity.value,
+                onChanged: (value) {
+                  exampletwocontroller.setOpacity(value);
+                });
+          }
+          )
+        ],
+      ),
+    );
+  }
+}
